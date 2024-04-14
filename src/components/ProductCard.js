b1b7a3a0083b9e3c06a70f7ccf03efa3e69c358a
Shivaram <127323182+sivaram6411@users.noreply.github.com>
2024-04-14 22:25:32 +0530

    Update ProductCard.js

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -4,8 +4,6 @@ import { MdCurrencyRupee } from "react-icons/md";
 import { TbShoppingBagPlus } from "react-icons/tb";
 import { ProductsList } from "../utils/Constants";
 import BagCategories from "./BagCategories";
-import { useEffect, useState } from "react";
-import axios from "axios";
 
 const ProductCard = () => {
   return (
@@ -66,74 +64,5 @@ const ProductCard = () => {
 
 export default ProductCard;
 
-{
-  /*
-  * this data was using Axios
-  
-  const ProductCard = () => {
-  const [bagList, setBagList] = useState([]);
 
-  useEffect(() => {
-    axios
-      .get("http://localhost:3030/data")
-      .then((res) => setBagList(res.data))
-      .catch((err) => console.log(err));
-  }, []);
 
-  return (
-    <div>
-      <BagCategories />
-      <div className="flex justify-between font-medium mx-5">
-        <p className="text-gray-300">
-          Bags <span className="text">.</span> Backpacks
-        </p>
-        <p className="text-gray-300 flex">
-          13 products{"  "}
-          <span className="text-xl my-1 mx-1">
-            <BsBoxArrowUp />
-          </span>
-        </p>
-      </div>
-      <ul className="flex flex-wrap">
-        {bagList.map((product) => {
-          return (
-            <li key={product.id}>
-              <div className="bg-gray-800 w-64 rounded-md mx-4 mt-5">
-                <div className="relative">
-                  <CiBookmark className="absolute right-0 m-2 text-3xl" />
-                  <img
-                    src={product.cardImage}
-                    className="w-auto rounded-t-md"
-                    alt="bagImage"
-                  />
-                </div>
-                <p className="text-gray-300 text-lg mx-4 mt-5 mb-2">
-                  {product.content}
-                </p>
-                <div className="text-white flex justify-between">
-                  <div className="flex mx-4 mb-5">
-                    <MdCurrencyRupee className="mt-1 font-bold text-lg" />
-                    <p className="text-white text-lg font-medium mt-0">
-                      {product.offerPrice}{" "}
-                      <span className="text-xs font-extralight line-through">
-                        {product.originalPrice}
-                      </span>{" "}
-                      <span className="text-xs text-green-400">
-                        {product.discount}
-                      </span>
-                    </p>
-                  </div>
-                  <div className="items-center text-4xl mr-5">
-                    <TbShoppingBagPlus />
-                  </div>
-                </div>
-              </div>
-            </li>
-          );
-        })}
-      </ul>
-    </div>
-  );
-};
- */
-}
